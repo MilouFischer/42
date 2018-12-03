1f4c6e2c567b9b59ff1d64c583b5e85b15e55918
Etienne FISCHER <efischer@e1r2p17.42.fr>
2018-12-03 11:52:20 +0100

    modif strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: efischer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 16:19:21 by efischer          #+#    #+#             */
-/*   Updated: 2018/11/12 16:45:45 by efischer         ###   ########.fr       */
+/*   Updated: 2018/11/28 14:54:53 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char			*str;
 
 	i = 0;
-	if (!s1 || !s2)
+	if (!s1 && !s2)
 		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
 	if (!(str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)
 		+ 1))))
 		return (NULL);
