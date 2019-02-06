cf02df5a0f00c07183a3f8b0b825f3895fb73042
Etienne Fischer <etienne.fischer77@gmail.com>
2019-02-06 10:45:51 -0800

    up

--- cursus_42/algo/ft_printf/ft_manage_conv.c
+++ cursus_42/algo/ft_printf/ft_manage_conv.c
@@ -60,7 +60,10 @@ char	*ft_manage_str(char c, char	*format, va_list *arg, t_flag *flag)
 			while (len)
 				tmp[len--] = ' ';
 			tmp[len] = ' ';
-			s = ft_join_free(tmp, s, 2);
+			if (flag->min)
+				s = ft_join_free(s, tmp, 1);
+			else
+				s = ft_join_free(tmp, s, 2);
 			ft_strdel(&tmp);
 		}
 		format = ft_strdup(s);
