6562366f4c60adbef540a5dea9decbaa8811c89c
Etienne Fischer <etienne.fischer77@gmail.com>
2019-03-11 03:44:39 -0700

    up

--- cursus_42/algo/ft_printf/ft_printf.c
+++ cursus_42/algo/ft_printf/ft_printf.c
@@ -22,15 +22,11 @@ static char		*ft_percent(char c, t_flag *flag)
 	return (format);
 }
 
-static char		*ft_diouxxf(char c, va_list *arg, t_flag *flag, char *tmp)
+static char		*ft_diouxxf(char c, va_list *arg, t_flag *flag)
 {
-	char	*str;
 	char	*format;
 
-	str = ft_manage_conv(c, arg, flag);
-	format = ft_strjoin(tmp, str);
-	ft_strdel(&tmp);
-	ft_strdel(&str);
+	format = ft_manage_conv(c, arg, flag);
 	if (flag->sharp > 0 && (c == 'x' || c == 'X') && *format != '0'
 	&& !flag->zero)
 	{
@@ -62,17 +58,54 @@ static void		ft_precision_width(t_flag *flag, char **s)
 	(*s)--;
 }
 
-static char		*ft_process_flag(char **s, va_list *arg, t_flag *flag)
+static char		*ft_flag_error(char *s, t_flag *flag)
 {
 	char	*format;
 	char	*tmp;
 	int		len;
 
-	format = NULL;
+	format = ft_strndup(s, 1);
+	if ((len = flag->width - ft_strlen(format)) > 0)
+	{
+		if (!(tmp = (char*)malloc(sizeof(char) * (len + 1))))
+		{
+			ft_strdel(&format);
+			return (NULL);
+		}
+		tmp[len--] = '\0';
+		while (len >= 0)
+			tmp[len--] = ' ';
+		if (flag->min)
+			format = ft_join_free(format, tmp, 1);
+		else
+			format = ft_join_free(tmp, format, 2);
+		ft_strdel(&tmp);
+	}
+	return (format);
+}
+
+static char		*ft_all_conv(char c, va_list *arg, t_flag *flag)
+{
+	char	*format;
+
+	format = NULL;	
+	if (c == 'c' || c == 's' || c == 'p')
+	{
+		format = ft_manage_str(c, format, arg, flag);
+		return (format);
+	}
+	else if (c == 'd' || c == 'i' || c == 'o' || c == 'u' || c == 'x'
+	|| c == 'X' || c == 'f')
+		return (ft_diouxxf(c, arg, flag));
+	else
+		return (ft_strdup("Z"));
+}
+
+static char		*ft_process_flag(char **s, va_list *arg, t_flag *flag)
+{
 	(*s)++;
 	while (**s)
 	{
-		tmp = format;
 		if (**s == '#' || **s == '0' || **s == '-' || **s == '+' || **s == ' '
 		|| **s == '.')
 			ft_manage_flag(**s, flag);
@@ -80,39 +113,14 @@ static char		*ft_process_flag(char **s, va_list *arg, t_flag *flag)
 			return (ft_percent(**s, flag));
 		else if (**s == 'h' || **s == 'l' || **s == 'L')
 			ft_manage_conv_flag(**s, flag);
-		else if (**s == 'c' || **s == 's' || **s == 'p')
-		{
-			format = ft_manage_str(**s, format, arg, flag);
-			return (format);
-		}
 		else if (**s == 'd' || **s == 'i' || **s == 'o' || **s == 'u'
-		|| **s == 'x' || **s == 'X' || **s == 'f')
-			return (ft_diouxxf(**s, arg, flag, tmp));
+		|| **s == 'x' || **s == 'X' || **s == 'f' || **s == 'c' || **s == 's'
+		|| **s == 'p' || **s == 'Z')
+			return (ft_all_conv(**s, arg, flag));
 		else if (**s >= '1' && **s <= '9')
 			ft_precision_width(flag, s);
-		else if (**s == 'Z')
-			return (ft_strdup("Z"));
 		else
-		{
-			format = ft_strndup(*s, 1);
-			if ((len = flag->width - ft_strlen(format)) > 0)
-			{
-				if (!(tmp = (char*)malloc(sizeof(char) * (len + 1))))
-				{
-					ft_strdel(&format);
-					return (NULL);
-				}
-				tmp[len--] = '\0';
-				while (len >= 0)
-					tmp[len--] = ' ';
-				if (flag->min)
-					format = ft_join_free(format, tmp, 1);
-				else
-					format = ft_join_free(tmp, format, 2);
-				ft_strdel(&tmp);
-			}
-			return (format);
-		}
+			return (ft_flag_error(*s, flag));
 		(*s)++;
 	}
 	return (ft_strdup("\0"));
