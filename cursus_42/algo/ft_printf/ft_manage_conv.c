9e603baa271ff8546da6e16bb953a02e5b2028a6
Etienne Fischer <etienne.fischer77@gmail.com>
2019-03-11 08:25:05 -0700

    up

--- cursus_42/algo/ft_printf/ft_manage_conv.c
+++ cursus_42/algo/ft_printf/ft_manage_conv.c
@@ -303,45 +303,47 @@ char	*ft_diouxx(char c, va_list *arg, t_flag *flag)
 		return (NULL);
 }
 
-char	*ft_long_diouxx(char c, va_list *arg, t_flag *flag)
+static char		*ft_manage_long_o(va_list *arg, t_flag *flag)
+{
+	unsigned long	u;
+	char			*s;
+
+	u = va_arg(*arg, unsigned long);
+	s = ft_itoa_base_u(u, 8);
+	if (flag->sharp)
+		s = ft_join_free("0", s, 2);
+	return (s);
+}
+
+static char		*ft_manage_long_diu(char c, va_list *arg)
 {
 	long long int	nb;
 	unsigned long	u;
-	char			*s;
-	char			*tmp;
 
-	if (c == 'd')
+	if (c == 'd' || c == 'i')
 	{
 		nb = va_arg(*arg, long long int);
 		return (ft_itoa_base(nb, 10));
 	}
-	else if (c == 'i')
-	{
-		nb = va_arg(*arg, long long int);
-		return (ft_itoa_base_u(nb, 10));
-	}
-	else if (c == 'o')
-	{
-		u = va_arg(*arg, unsigned long);
-		s = ft_itoa_base_u(u, 8);
-		if (flag->sharp)
-		{
-			tmp = s;
-			s = ft_strjoin("0", tmp);
-			ft_strdel(&tmp);
-		}
-		return (s);
-	}
-	else if (c == 'u')
+	else
 	{
 		u = va_arg(*arg, unsigned long);
 		return (ft_itoa_base_u(u, 10));
 	}
+}
+
+char	*ft_long_diouxx(char c, va_list *arg, t_flag *flag)
+{
+	unsigned long	u;
+
+	if (c == 'd' || c == 'i' || c == 'u')
+		return (ft_manage_long_diu(c, arg));
+	else if (c == 'o')
+		return (ft_manage_long_o(arg, flag));
 	else if (c == 'x' || c == 'X')
 	{
 		u = va_arg(*arg, unsigned long);
-		s = ft_itoa_base_u(u, 16);
-		return (s);
+		return (ft_itoa_base_u(u, 16));
 	}
 	else
 		return (NULL);
