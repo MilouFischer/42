2de81a8866c90cf6f1a218a07395863c2933f93c
Etienne Fischer <etienne.fischer77@gmail.com>
2019-03-11 04:55:07 -0700

    up

--- cursus_42/algo/ft_printf/ft_manage_conv.c
+++ cursus_42/algo/ft_printf/ft_manage_conv.c
@@ -136,12 +136,52 @@ static char		*ft_manage_s(va_list *arg, t_flag *flag)
 	return (s);
 }
 
-static char		*ft_manage_p(va_list *arg, t_flag *flag)
+static char		*ft_p_precision(char *s, t_flag *flag)
 {
 	int		len;
+	char	*tmp;
+
+	if ((len = flag->precision - ft_strlen(s)) > 0)
+	{
+		if (!(tmp = (char*)malloc(sizeof(char) * (len + 1))))
+			return (NULL);
+		tmp[len--] = '\0';
+		while (len >= 0)
+			tmp[len--] = '0';
+		if (flag->min)
+			s = ft_join_free(s, tmp, 1);
+		else
+			s = ft_join_free(tmp, s, 2);
+		ft_strdel(&tmp);
+	}
+	return (s);
+}
+
+static char		*ft_p_width(char *s, t_flag *flag)
+{
+	int		len;
+	char	*tmp;
+
+	if ((len = flag->width - ft_strlen(s)) > 0)
+	{
+		if (!(tmp = (char*)malloc(sizeof(char) * (len + 1))))
+			return (NULL);
+		tmp[len--] = '\0';
+		while (len >= 0)
+			tmp[len--] = ' ';
+		if (flag->min)
+			s = ft_join_free(s, tmp, 1);
+		else
+			s = ft_join_free(tmp, s, 2);
+		ft_strdel(&tmp);
+	}
+	return (s);
+}
+
+static char		*ft_manage_p(va_list *arg, t_flag *flag)
+{
 	void	*p;
 	char	*s;
-	char	*tmp;
 
 	p = va_arg(*arg, void*);
 	if (flag->precision == -1)
@@ -149,38 +189,10 @@ static char		*ft_manage_p(va_list *arg, t_flag *flag)
 	else
 		s = ft_itoa_base_u((unsigned long)p, 16);
 	if (flag->precision)
-	{
-		if ((len = flag->precision - ft_strlen(s)) > 0)
-		{
-			if (!(tmp = (char*)malloc(sizeof(char) * (len + 1))))
-				return (NULL);
-			tmp[len--] = '\0';
-			while (len >= 0)
-				tmp[len--] = '0';
-			if (flag->min)
-				s = ft_join_free(s, tmp, 1);
-			else
-				s = ft_join_free(tmp, s, 2);
-			ft_strdel(&tmp);
-		}
-	}
+		s = ft_p_precision(s, flag);
 	s = ft_join_free("0x", s, 2);
 	if (flag->width)
-	{
-		if ((len = flag->width - ft_strlen(s)) > 0)
-		{
-			if (!(tmp = (char*)malloc(sizeof(char) * (len + 1))))
-				return (NULL);
-			tmp[len--] = '\0';
-			while (len >= 0)
-				tmp[len--] = ' ';
-			if (flag->min)
-				s = ft_join_free(s, tmp, 1);
-			else
-				s = ft_join_free(tmp, s, 2);
-			ft_strdel(&tmp);
-		}
-	}
+		s = ft_p_width(s, flag);
 	return (s);
 }
 
