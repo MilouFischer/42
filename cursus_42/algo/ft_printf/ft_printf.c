e8819197e32be3a84dafda331c332147acdc55fb
MilouFischer <etienne.fischer77@gmail.com>
2019-03-18 14:12:14 +0100

    up

--- cursus_42/algo/ft_printf/ft_printf.c
+++ cursus_42/algo/ft_printf/ft_printf.c
@@ -35,43 +35,36 @@ static t_list	*ft_fill_content(t_list *lst, char *str, t_flag flag)
 	return (lst);
 }
 
-static t_list	*ft_fill_list(t_list *lst, char *str, t_flag flag)
-{
-	t_list	*new;
-
-	if (!lst->content)
-		return (ft_fill_content(lst, str, flag));
-	new = ft_lstnew(NULL, 0);
-	new = ft_fill_content(new, str, flag);
-	ft_lstadd(&new, lst);
-	return (lst);
-}
-
 static t_list	*ft_get_flags(t_list *list, va_list *arg, char *str)
 {
 	char	*tmp;
 	char	*format;
+	t_list	*new;
 	t_list	*tmp_list;
 	t_flag	flag;
 
-	(void)arg;
 	tmp_list = list;
+	ft_strdel(&list->content);
 	while ((format = ft_strchr(str, '%')))
 	{
 		ft_init_flag(&flag);
-		tmp = str;
-		tmp[format - str] = '\0';
-		list = ft_fill_list(list, tmp, flag);
-		tmp = str;
+		list = ft_fill_content(list, ft_strsub(str, 0, format - str), flag);
+		new = ft_lstnew_str(NULL, 0);
+		ft_lstadd(&new, list);
+		list = list->next;
 		str = ft_strdup(format);
-		ft_strdel(&tmp);
-		list = list->next;
-		/*if (!(tmp = ft_process_flag(&str, arg, &flag)))
+		if (!(tmp = ft_process_flag(&str, arg, &flag)))
 			return (NULL);
-		list = ft_fill_list(list, tmp, flag);
 		str++;
+		list = ft_fill_content(list, tmp, flag);
+		new = ft_lstnew_str(NULL, 0);
+		ft_lstadd(&new, list);
 		list = list->next;
-		flag.null = 0;*/
+	}
+	if (*str)
+	{
+		ft_init_flag(&flag);
+		list = ft_fill_content(list, str, flag);
 	}
 	return (tmp_list);
 }
@@ -85,7 +78,7 @@ int				ft_printf(const char *format, ...)
 
 	if (!format)
 		return (0);
-	list = ft_lstnew(NULL, 0);
+	list = ft_lstnew(format, ft_strlen(format));
 	str = ft_strdup(format);
 	if (ft_strchr(str, '%'))
 	{
