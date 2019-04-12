573d9db1715ccb1848c79d71302fd888075b3014
Etienne FISCHER <efischer@e1r2p17.42.fr>
2019-04-12 17:31:15 +0200

    up

--- cursus_42/algo/ft_printf/ft_width_precision.c
+++ cursus_42/algo/ft_printf/ft_width_precision.c
@@ -6,7 +6,7 @@
 /*   By: efischer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 14:36:23 by efischer          #+#    #+#             */
-/*   Updated: 2019/04/12 17:12:25 by efischer         ###   ########.fr       */
+/*   Updated: 2019/04/12 17:28:15 by efischer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,45 +62,12 @@ char			*ft_precision(char conv, char *format, t_flag *flag)
 	return (format);
 }
 
-static char		*ft_process_str(int len, char x, char *format, t_flag *flag)
+static char		*ft_process_str(char *str, char x, char *format, t_flag *flag)
 {
-	int		i;
-	char	c;
-	char	*str;
-
-	i = 0;
-	if (!(str = (char*)malloc(sizeof(char) * (len + 1))))
-	{
-		ft_strdel(&format);
-		return (NULL);
-	}
-	c = ' ';
-	if (flag->zero && !flag->min)
-		c = '0';
-	while (i < len)
-		str[i++] = c;
-	str[i] = '\0';
-	if (flag->plus && c == '0')
+	if (flag->plus && flag->zero && !flag->min)
 		str = ft_join_free("+", str, 2);
-	if (flag->sharp && (x == 'x' || x == 'X') && c == '0')
+	if (flag->sharp && (x == 'x' || x == 'X') && flag->zero && !flag->min)
 		str = ft_join_free("0x", str, 2);
-	return (str);
-}
-
-static char		*ft_process_width(int len, char *format, char x, t_flag *flag)
-{
-	char	*str;
-
-	if (flag->plus)
-		len--;
-	if (!(flag->zero && !flag->min))
-	{
-		if (flag->plus)
-			format = ft_join_free("+", format, 2);
-		if (flag->sharp && (x == 'x' || x == 'X'))
-			format = ft_join_free("0x", format, 2);
-	}
-	str = ft_process_str(len, x, format, flag);
 	if (flag->space && flag->min && *format != '-')
 		format = ft_join_free(" ", format, 2);
 	else if (flag->space)
@@ -113,13 +80,43 @@ static char		*ft_process_width(int len, char *format, char x, t_flag *flag)
 	if (flag->min && flag->null)
 	{
 		ft_strdel(&format);
-		format = ft_memjoin("\0", str, 1, len);
+		format = ft_memjoin("\0", str, 1, ft_strlen(str));
 		ft_strdel(&str);
 	}
 	else if (flag->min)
 		format = ft_join_free(format, str, 3);
 	else
 		format = ft_join_free(str, format, 1);
+	return (format);
+}
+
+static char		*ft_process_width(int len, char *format, char x, t_flag *flag)
+{
+	char	*str;
+	char	c;
+	int		i;
+
+	if (flag->plus)
+		len--;
+	c = '0';
+	if (!(flag->zero && !flag->min))
+	{
+		c = ' ';
+		if (flag->plus)
+			format = ft_join_free("+", format, 2);
+		if (flag->sharp && (x == 'x' || x == 'X'))
+			format = ft_join_free("0x", format, 2);
+	}
+	i = 0;
+	if (!(str = (char*)malloc(sizeof(char) * (len + 1))))
+	{
+		ft_strdel(&format);
+		return (NULL);
+	}
+	while (i < len)
+		str[i++] = c;
+	str[i] = '\0';
+	format = ft_process_str(str, x, format, flag);
 	return (format);
 }
 
